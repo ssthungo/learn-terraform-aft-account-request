603a03d334df92b8f1bceb2ae6e35f5a613e8244
ssthungo <ssthungo@amazon.com>
2026-04-21 15:18:01 +0200

    Create aft-import.tf

---
+++ terraform/aft-import.tf
@@ -0,0 +1,23 @@
+module "aft-import" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ssthungo+aftimport@amazon.com"
+    AccountName               = "aft-import"
+    ManagedOrganizationalUnit = "Sandbox-CT"
+    SSOUserEmail              = "ssthungo+aftimport@amazon.com"
+    SSOUserFirstName          = "AFT-Import"
+    SSOUserLastName           = "Test"
+  }
+
+  account_tags = {}
+
+  change_management_parameters = {
+    change_requested_by = "AFT"
+    change_reason       = "Enroll existing AFT-Import account into AFT"
+  }
+
+  custom_fields = {}
+
+  account_customizations_name = "aft-import"
+}
