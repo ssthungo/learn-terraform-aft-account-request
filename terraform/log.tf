fbf3e4a1dda75838f8e672bf8b0b0499e3909740
ssthungo <ssthungo@amazon.com>
2026-07-24 15:10:30 +0200

    Create log.tf

---
+++ terraform/log.tf
@@ -0,0 +1,23 @@
+module "aft-log-acc" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ssthungo+log@amazon.com"
+    AccountName               = "Log Archive"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "ssthungo+log@amazon.com"
+    SSOUserFirstName          = "Log"
+    SSOUserLastName           = "Archive"
+  }
+
+  account_tags = {}
+
+  change_management_parameters = {
+    change_requested_by = "AFT"
+    change_reason       = "Enroll existing AFT-Import account into AFT"
+  }
+
+  custom_fields = {}
+
+  account_customizations_name = ""
+}
