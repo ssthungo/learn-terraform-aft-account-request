2ad008457c3da83312c4eda57f6ac2cf09585731
ssthungo <ssthungo@amazon.com>
2026-09-04 11:06:21 +0200

    Update account-workflow-check.tf

--- terraform/account-workflow-check.tf
+++ terraform/account-workflow-check.tf
@@ -4,7 +4,7 @@ module "account-workflow" {
   control_tower_parameters = {
     AccountEmail              = "ssthungo+aft+wrkflow@amazon.com"
     AccountName               = "aft-account-wf"
-    ManagedOrganizationalUnit = "SST"
+    ManagedOrganizationalUnit = "Sandbox-CT"
     SSOUserEmail              = "ssthungo+wrkflow@amazon.com"
     SSOUserFirstName          = "aft-account-wf"
     SSOUserLastName           = "AFT"
